4fe337593c8ee0135a0a7b302d4f752c81dfefc9
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-17 17:56:55 +0900

    [finish]rails-tutorial3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
+# rails-tutorial
+	root 'static_pages#home'
+
+
+
+# 多分ここから下はDMMのやつ
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'

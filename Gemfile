973c8c9abee40afd08605d688a1ae4524995cd2f
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-19 11:06:33 +0900

    [finish]chapter5

--- Gemfile
+++ Gemfile
@@ -76,6 +76,7 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 # 画像加工用（サイズ調整など）gem
 gem "refile-mini_magick"
 
+gem 'bootstrap-sass', '3.3.7'
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get  '/signup',  to: 'users#new'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
 # rails-tutorial
-	root 'static_pages#home'
-
 
 
 # 多分ここから下はDMMのやつ

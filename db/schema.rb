170f8fc9c545c25447ad0ef9e44ef9bec99cfd78
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-19 12:21:35 +0900

    [finish]chapter6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_024509) do
+ActiveRecord::Schema.define(version: 2020_11_19_024551) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_05_20_024509) do
     t.string "title"
     t.text "body"
     t.string "image_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

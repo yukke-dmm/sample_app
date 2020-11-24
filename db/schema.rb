5fe0bd416a359a626da7ba766d2c37551396cde9
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-24 17:20:24 +0900

    [finish]chapter12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_042348) do
+ActiveRecord::Schema.define(version: 2020_11_24_080757) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_11_24_042348) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

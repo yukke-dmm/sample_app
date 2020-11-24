72d0217865ed191ae4b23aa984cf3cbc144d2df7
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-24 18:42:50 +0900

    [finish]chapter114

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_082233) do
+ActiveRecord::Schema.define(version: 2020_11_24_092058) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,8 +26,19 @@ ActiveRecord::Schema.define(version: 2020_11_24_082233) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|

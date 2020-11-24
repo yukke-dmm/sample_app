2ee751bd34126f1b0c807bd1b745f2c52463f84a
YUSUKE MORITA <yukke.dwc.0516@gmail.com>
2020-11-24 18:17:53 +0900

    [finish]chapter13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_080757) do
+ActiveRecord::Schema.define(version: 2020_11_24_082233) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_11_24_080757) do
     t.string "title"
     t.text "body"
     t.string "image_id"
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
